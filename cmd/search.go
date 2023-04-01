6bbb0c2d3e95b3227e45e2ae4a7d98750f8346fd
devnote-dev <devnote.dev75@gmail.com>
2023-04-01 22:23:56 +0100

    feat(cmd): search progress

--- cmd/search.go
+++ cmd/search.go
@@ -2,14 +2,47 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
-	"github.com/devnote-dev/docr/search"
+	"github.com/devnote-dev/docr/env"
 	"github.com/spf13/cobra"
 )
 
 var searchCommand = &cobra.Command{
 	Use: "search symbol",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%v\n", search.ParseInfo(args))
+		if len(args) == 0 {
+			fmt.Fprintln(os.Stderr, "no arguments provided")
+			return
+		}
+
+		module := ""
+		symbol := args[0]
+		if len(args) > 1 {
+			module = symbol
+			// symbol = args[1]
+		}
+
+		if module == "" {
+			versions, err := env.GetLibraryVersions("crystal")
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+
+			if len(versions) == 0 {
+				fmt.Fprintln(os.Stderr, "latest crystal standard library documentation is not available")
+				return
+			}
+
+			latest := versions[len(versions)-1]
+			lib, err := env.GetLibrary("crystal", latest)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+
+			fmt.Printf("%v\n", lib)
+		}
 	},
 }
