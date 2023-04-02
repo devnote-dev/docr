c737094bf08b9a6269eea33ecfa001c3ef1d97b2
devnote-dev <devnote.dev75@gmail.com>
2023-04-02 22:23:43 +0100

    refactor(search): implement individual sort functions

--- search/search.go
+++ search/search.go
@@ -1,31 +1,130 @@
 package search
 
-import "strings"
+import (
+	"sort"
 
-type SearchInfo struct {
-	module string
-	symbol string
-	field  string
+	"github.com/devnote-dev/docr/crystal"
+)
+
+type Result struct {
+	Name   string
+	Source *crystal.Location
 }
 
-func ParseInfo(args []string) SearchInfo {
-	var module, symbol, field string
+type constants []*crystal.Constant
 
-	if strings.Contains(args[0], "::") {
-		parts := strings.Split(args[0], "::")
-		symbol = parts[len(parts)-1]
-		module = strings.Join(parts, "::")
-	} else if strings.Contains(args[0], ".") {
-		parts := strings.Split(args[0], ".")
-		field = parts[len(parts)-1]
+func (c constants) Len() int           { return len(c) }
+func (c constants) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c constants) Less(i, j int) bool { return c[i].Name < c[j].Name }
+
+type definitions []*crystal.Definition
+
+func (d definitions) Len() int           { return len(d) }
+func (d definitions) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+func (d definitions) Less(i, j int) bool { return d[i].Name < d[j].Name }
+
+type types []*crystal.Type
+
+func (t types) Len() int           { return len(t) }
+func (t types) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+func (t types) Less(i, j int) bool { return t[i].FullName < t[j].FullName }
+
+func FindConstants(lib *crystal.Type, symbol string) []*Result {
+	if lib.Constants == nil {
+		return nil
 	}
 
-	if len(args) > 1 {
-		if strings.Contains(args[1], ".") {
-			parts := strings.Split(args[1], ".")
-			field = parts[len(parts)-1]
-		}
+	if len(lib.Constants) == 0 {
+		return nil
 	}
 
-	return SearchInfo{module, symbol, field}
+	consts := (constants)(lib.Constants)
+	sort.Sort(consts)
+
+	r := make([]*Result, len(lib.Constants))
+	for i, c := range consts {
+		r[i] = &Result{Name: c.Name, Source: nil}
+	}
+
+	return r
+}
+
+func FindConstructors(lib *crystal.Type, symbol string) []*Result {
+	if lib.Constructors == nil {
+		return nil
+	}
+
+	if len(lib.Constructors) == 0 {
+		return nil
+	}
+
+	defs := (definitions)(lib.Constructors)
+	sort.Sort(defs)
+
+	r := make([]*Result, len(lib.Constructors))
+	for i, c := range defs {
+		r[i] = &Result{Name: c.Name, Source: nil}
+	}
+
+	return r
+}
+
+func FindMethods(lib *crystal.Type, symbol string) []*Result {
+	if lib.ClassMethods == nil {
+		return nil
+	}
+
+	if len(lib.ClassMethods) == 0 {
+		return nil
+	}
+
+	defs := (definitions)(lib.ClassMethods)
+	sort.Sort(defs)
+
+	r := make([]*Result, len(lib.ClassMethods))
+	for i, d := range defs {
+		r[i] = &Result{Name: d.Name, Source: d.Location}
+	}
+
+	return r
+}
+
+func FindMacros(lib *crystal.Type, symbol string) []*Result {
+	if lib.Macros == nil {
+		return nil
+	}
+
+	if len(lib.Macros) == 0 {
+		return nil
+	}
+
+	defs := (definitions)(lib.Macros)
+	sort.Sort(defs)
+
+	r := make([]*Result, len(lib.Macros))
+	for i, m := range defs {
+		r[i] = &Result{Name: m.Name, Source: m.Location}
+	}
+
+	return r
+}
+
+func FindTypes(lib *crystal.Type, symbol string) []*Result {
+	if lib.Types == nil {
+		return nil
+	}
+
+	if len(lib.Types) == 0 {
+		return nil
+	}
+
+	defs := (types)(lib.Types)
+	sort.Sort(defs)
+
+	r := make([]*Result, len(lib.Types))
+	for i, t := range defs {
+		r[i] = &Result{Name: t.FullName, Source: t.Locations[0]}
+	}
+
+	return r
 }
