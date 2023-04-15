b7af73a6639b9740b39c9c6e9720c47e398a8407
devnote-dev <devnote.dev75@gmail.com>
2023-04-15 19:59:57 +0100

    feat(scripts): use powershell, include build date

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,8 +1,2 @@
 @ECHO OFF
-SETLOCAL
-
-FOR /F %%i IN ('git rev-parse HEAD') DO SET BUILD=%%i
-SET GOOS=windows
-go build -ldflags="-X github.com/devnote-dev/docr/cmd.Build=%BUILD%" -o build\docr.exe
-
-ENDLOCAL
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "%~dp0build.ps1"
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 GIT_HEAD=$(git rev-parse HEAD | head -c8)
-GOOS=linux go build -ldflags "-X github.com/devnote-dev/docr/cmd.Build=$GIT_HEAD" -o build/docr
+GOOS=linux go build -ldflags "-X github.com/devnote-dev/docr/cmd.Build=$GIT_HEAD -X github.com/devnote-dev/docr/cmd.Date=$(date +%F)" -o build/docr
