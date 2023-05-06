03d1c4df0d586275837546960049a9fbc6a53de6
devnote-dev <devnote.dev75@gmail.com>
2023-05-06 02:38:10 +0100

    feat(commands): include init command for env

--- src/commands/env.cr
+++ src/commands/env.cr
@@ -8,6 +8,11 @@ module Docr::Commands
         will print that environment value to the terminal.
         DESC
 
+      add_usage "env [options]"
+      add_usage "env init [options]"
+
+      add_command Init.new
+
       add_argument "name", description: "the name of the env variable", required: false
     end
 
@@ -28,8 +33,24 @@ module Docr::Commands
         return
       end
 
-      stdout << "DOCR_CACHE=" << Library::CACHE_DIR << '\n'
-      stdout << "DOCR_LIBRARY=" << Library::LIBRARY_DIR << '\n'
+      warn_cache = Dir.exists?(Library::CACHE_DIR) ? "" : " (!)".colorize.yellow
+      lib_cache = Dir.exists?(Library::LIBRARY_DIR) ? "" : " (!)".colorize.yellow
+
+      stdout << "DOCR_CACHE=" << Library::CACHE_DIR << warn_cache << '\n'
+      stdout << "DOCR_LIBRARY=" << Library::LIBRARY_DIR << lib_cache << '\n'
+    end
+
+    class Init < Base
+      def setup : Nil
+        @name = "init"
+        @summary = "initializes the environment"
+        @description = "Creates the required files and directories for Docr to run."
+      end
+
+      def run(arguments : Cling::Arguments, options : Cling::Options) : Nil
+        Dir.mkdir_p Library::CACHE_DIR unless Dir.exists? Library::CACHE_DIR
+        Dir.mkdir_p Library::LIBRARY_DIR unless Dir.exists? Library::LIBRARY_DIR
+      end
     end
   end
 end
