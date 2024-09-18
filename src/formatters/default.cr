6669cd043c294530c1006f2d00aefcb107710b1b
devnote-dev <devnote.dev75@gmail.com>
2024-09-18 11:44:33 +0100

    feat(formatters): major markdown rendering improvements

--- src/formatters/default.cr
+++ src/formatters/default.cr
@@ -1,51 +1,59 @@
 module Docr::Formatters::Default
   class Renderer < Markd::Renderer
-    def heading(node : Markd::Node, __) : Nil
+    def heading(node : Markd::Node, entering : Bool) : Nil
       level = node.data["level"].as(Int32)
-      if level == 1
-        literal(String.build do |str|
-          Colorize.with.back(:magenta).fore(:white).bold.surround(str) do |io|
-            io << ' ' << node.text << ' '
-          end
-        end)
+
+      if entering
+        literal "\n"
+
+        if level == 1
+          literal "\e[45;97m  "
+        else
+          literal "\e[34m"
+          literal "#" * level
+          literal " "
+        end
       else
-        literal(String.build do |str|
-          Colorize.with.bold.fore(:white).surround(str) do |io|
-            io << "#" * level
-            io << ' ' << node.text
-          end
-        end)
+        literal "  " if level == 1
+        literal "\e[0m\n\n"
       end
     end
 
     def code(node : Markd::Node, __) : Nil
-      literal(String.build do |str|
-        Colorize.with.back(236).fore(203).surround(str) do |io|
+      literal(String.build do |io|
+        Colorize.with.back(236).fore(203).surround(io) do
           io << ' ' << node.text << ' '
         end
       end)
     end
 
     def code_block(node : Markd::Node, __) : Nil
-      literal(String.build do |str|
-        str << "\n\n"
-        node.text.each_line do |line|
-          str << "  " << line << '\n'
+      literal(String.build do |io|
+        io << '\n'
+        Colorize.with.fore(244).surround(io) do
+          node.text.each_line do |line|
+            io << "  " << line << '\n'
+          end
         end
-        str << '\n'
+        io << '\n'
       end)
     end
 
     def thematic_break(node : Markd::Node, __) : Nil
+      literal "\n————————————\n".colorize.dark_gray.to_s
     end
 
-    def block_quote(node : Markd::Node, __) : Nil
+    def block_quote(node : Markd::Node, entering : Bool) : Nil
+      # literal "┃ ".colorize.dark_gray.to_s if entering
+      literal "\n" unless entering
     end
 
-    def list(node : Markd::Node, __) : Nil
+    def list(node : Markd::Node, entering : Bool) : Nil
+      literal "\n" unless entering
     end
 
-    def item(node : Markd::Node, __) : Nil
+    def item(node : Markd::Node, entering : Bool) : Nil
+      literal "• " if entering
     end
 
     def link(node : Markd::Node, __) : Nil
@@ -62,11 +70,11 @@ module Docr::Formatters::Default
     end
 
     def html_inline(node : Markd::Node, __) : Nil
-      literal node.text
+      literal "\n"
     end
 
-    def paragraph(node : Markd::Node, __) : Nil
-      literal node.text
+    def paragraph(node : Markd::Node, entering : Bool) : Nil
+      literal "\n" unless entering
     end
 
     def emphasis(node : Markd::Node, __) : Nil
@@ -74,7 +82,7 @@ module Docr::Formatters::Default
     end
 
     def soft_break(node : Markd::Node, __) : Nil
-      literal "\n"
+      literal " "
     end
 
     def line_break(node : Markd::Node, __) : Nil
