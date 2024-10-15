9809b55a8a6ceab8ff1f68f0ed94ef4217ca1c99
devnote-dev <devnote.dev75@gmail.com>
2024-10-15 23:31:38 +0100

    fix(commands): sort versions semantically, handle non-versions

--- src/commands/check.cr
+++ src/commands/check.cr
@@ -18,6 +18,7 @@ module Docr::Commands
         Library.list_all.each do |name, versions| # ameba:disable Lint/ShadowingOuterLocalVar
           stdout.puts name
           check name, versions
+          stdout.puts
         end
       end
     end
@@ -25,6 +26,7 @@ module Docr::Commands
     private def check(name : String, installed : Array(String)) : Nil
       url = Library.get_source name
       res = Crest.get url + "/versions.json"
+      max = SemanticVersion.new(10, 0, 0)
 
       Array({name: String})
         .from_json(res.body, root: "versions")
@@ -32,7 +34,7 @@ module Docr::Commands
         .reject(&.in? installed)
         .map { |v| {v, false} }
         .concat(installed.map { |v| {v, true} })
-        .sort_by!(&.[0])
+        .sort_by! { |(v, _)| SemanticVersion.parse(v) rescue max }
         .reverse_each do |(name, added)|
           if added
             Colorize.with.green.surround(stdout) do
